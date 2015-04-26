7223d45d909a3627262d84c4da2f51a7d1fc78fd
Ben Dunkle <ben@field2.com>
2015-04-26 11:19:17 -0400

    Added the table of contacts using the mysql_query and mysql_fetch_row functions
    Added more comments

--- contact.php
+++ contact.php
@@ -1,32 +1,24 @@
 <?php
-
 //assign credentials. These will work if you're using mamp and localhost. Change them to the credentials you used when you installed wordpress on your server
 $host="localhost";
 $username="root";
 $password="root";
-
 // connect to database
 $connect = mysql_connect($host, $username, $password)
 or die (mysql_error());
-
 //if the user has submitted the form, store all the variables in the $_POST superglobal array.
-if(isset($_POST['submitted'])) { 
-    $firstname=$_POST['firstname'];
-    $lastname=$_POST['lastname'];
-    $email=$_POST['email'];
-    $comments=$_POST['comments'];
-
-    //select the database to read from. If you created the contacts table in your wordpress DB, it's the same one you used when you set uop wordpress
-    mysql_select_db("contacts", $connect)
-    or die (mysql_error());
-
-    //insert the submitted data into the database
-    mysql_query ("INSERT into contacts (firstname, lastname, email, comments) VALUES ('$firstname', '$lastname', '$email', '$comments')", $connect)
-    or die (mysql_error());
+if(isset($_POST['submitted'])) {
+$firstname=$_POST['firstname'];
+$lastname=$_POST['lastname'];
+$email=$_POST['email'];
+$comments=$_POST['comments'];
+//select the database to read from. If you created the contacts table in your wordpress DB, it's the same one you used when you set uop wordpress
+mysql_select_db("contacts", $connect)
+or die (mysql_error());
+//insert the submitted data into the database
+mysql_query ("INSERT into contacts (firstname, lastname, email, comments) VALUES ('$firstname', '$lastname', '$email', '$comments')", $connect)
+or die (mysql_error());
 }
-
-// close the connection
-mysql_close($connect);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -57,20 +49,38 @@ mysql_close($connect);
         </form>
     </section>
     <!-- #form -->
-
-  <?php if(isset($_POST['submitted'])) { //only show the thank you message and the list of contacts AFTER the user submits their info.
+    <?php if(isset($_POST['submitted'])) { //only show the thank you message and the list of contacts AFTER the user submits their info.
     //escape out of php ?>
     <section id="thanks">
         Thanks for your submission, <?php echo $firstname; /* display the user's first name */ ?>!
-        <br>
+        <h2>Contact list</h2>
         <table>
             <tr>
+                <th>Entry ID</th>
                 <th>First name</th>
                 <th>Last name</th>
                 <th>Email</th>
                 <th>Comments</th>
+                <th>Time of submission</th>
             </tr>
-            [loop through all the entries in the database and display as rows in the table]
+            <?php $contacts = mysql_query('SELECT * from contacts', $connect)
+            or die (mysql_error());
+            // loop through all the entries in the database and display as rows in the table
+            while ($row = mysql_fetch_row($contacts)) {
+            // echo the opening tab;e row tag
+            echo "<tr>";
+                //treat each field in the row as a single piece of data that we can echo out between <td> tags
+                foreach ($row as $field) { 
+                echo "<td>" . $field . "</td>";
+                }
+            // echo the closing table row tag
+            echo "</tr>";
+            }
+
+            // close the connection
+            mysql_close($connect);
+
+            ?>
         </table>
     </section>
     <?php // go back into php to close out the if statement with a curly bracket
